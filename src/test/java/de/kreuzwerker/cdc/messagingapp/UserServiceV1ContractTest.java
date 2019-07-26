253ac0f2676a92a6eb03066a5fcc880031b8b6e5
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2019-07-26 12:28:49 +0200

    changed contract

--- src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceV1ContractTest.java
+++ src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceV1ContractTest.java
@@ -43,7 +43,7 @@ public class UserServiceV1ContractTest {
                 .path("/users/1")
                 .method("GET")
                 .willRespondWith()
-                .status(300)
+                .status(200)
                 .body(LambdaDsl.newJsonBody((o) ->
                         o.stringType("name", "user name for CDC")
                 ).build()).toPact();
