c787a0cefe8982512a199b82d881d0ed06cd7f9a
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2019-07-29 12:13:27 +0200

    break api

--- src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceV1ContractTest.java
+++ src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceV1ContractTest.java
@@ -87,7 +87,7 @@ public class UserServiceV1ContractTest {
                 .path("/users/1")
                 .method("GET")
                 .willRespondWith()
-                .status(200)
+                .status(400)
                 .body(LambdaDsl.newJsonBody((o) ->
                         o.stringType("name", "user name for CDC")
                 ).build()).toPact();
