ce3dc90cd90b03e9682f6ee1e5903f284039251b
Eduardo Raupp Pereira <eduardo.raupp_pereira@roche.com>
2019-08-01 15:39:00 +0200

    fixed client

--- src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceV1ContractTest.java
+++ src/test/java/de/kreuzwerker/cdc/messagingapp/UserServiceV1ContractTest.java
@@ -95,20 +95,20 @@ public class UserServiceV1ContractTest {
                 .path("/users/1")
                 .method("GET")
                 .willRespondWith()
-               // .status(404)
-               // .toPact();
-                .status(200)
-                .body(LambdaDsl.newJsonBody((o) ->
-                        o.stringType("name", "user name for CDC")
-                ).build()).toPact();
+                .status(404)
+                .toPact();
+          //      .status(200)
+            //    .body(LambdaDsl.newJsonBody((o) ->
+              //          o.stringType("name", "user name for CDC")
+               // ).build()).toPact();
     }
 
     @PactVerification(fragment = "pactUserExists3")
     @Test
     public void userExists3() {
-       // expandException.expect(HttpClientErrorException.class);
-       // expandException.expectMessage("404 Not Found");
-        User user = userServiceClient.getUserOldVersion("1");
-        assertThat(user.getName()).isEqualTo("user name for CDC");
+       expandException.expect(HttpClientErrorException.class);
+       expandException.expectMessage("404 Not Found");
+        //User user = userServiceClient.getUserOldVersion("1");
+        //assertThat(user.getName()).isEqualTo("user name for CDC");
     }
 }
